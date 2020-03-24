b177faac965798f5ad96c646b68bb6bb48308a03
Fernanda D'Emery <nandademery2@hotmail.com>
2020-03-24 13:13:01 +0100

    Building Landing Page

--- react-youtube-app/client/src/components/views/LandingPage/LandingPage.js
+++ react-youtube-app/client/src/components/views/LandingPage/LandingPage.js
@@ -1,21 +1,63 @@
 import React from "react";
 import { FaCode } from "react-icons/fa";
 import { Card, Avatar, Col, Typography, Row } from "antd";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
+import moment from "moment";
 
 const { Title } = Typography;
 const { Meta } = Card;
 
 function LandingPage() {
+  const [Videos, setVideos] = useState([]);
+
   useEffect(() => {
     axios.get("/api/video/getVideos").then(response => {
       if (response.data.success) {
+        setVideos(response.data.videos);
       } else {
         alert("Failed to get videos");
       }
     });
   }, []);
+
+  const renderCards = Videos.map((video, index) => {
+    var minutes = Math.floor(video.duration / 60);
+    var seconds = Math.floor(video.duration - minutes * 60);
+
+    return (
+      <div>
+        <div
+          style={{
+            position: "relative"
+          }}
+        >
+          <img
+            style={{
+              width: "100%"
+            }}
+            src={`http://localhost:5000/${video.thumbnail}`}
+          />
+          <div className="duration">
+            <span> minute: second </span>{" "}
+          </div>{" "}
+        </div>{" "}
+        <Meta
+          avatar={<Avatar src={video.writer.image} />}
+          title={video.title}
+        />
+        <span> {video.writer.name} </span>{" "}
+        <span
+          style={{
+            marginLeft: "3rem"
+          }}
+        >
+          {video.views}
+        </span>{" "}
+        - <span> {moment(video.createdAt).format("MMM Do YY")} </span>{" "}
+      </div>
+    );
+  });
 
   return (
     <div
@@ -25,18 +67,7 @@ function LandingPage() {
       }}
     >
       <Title level={2}> Recommended </Title> <hr />
-      <div>
-        <div style={{ position: "relative" }}>
-          <img style={{ width: "100%" }} src />
-          <div className="duration">
-            <span>minute : second</span>
-          </div>
-        </div>
-        <Meta avatar={<Avatar src />} title />
-        <span>Writer name</span>
-        <span style={{ marginLeft: "3rem" }}>Views count</span> -{" "}
-        <span>date</span>
-      </div>
+      {renderCards}
     </div>
   );
 }
